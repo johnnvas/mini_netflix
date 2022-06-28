1650a673ff09438ab2f802813471667ff05ebf63
Johnn <j.christian.vasquez@gmail.com>
2022-06-27 22:41:15 -0500

    landing page

---
+++ src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+  return (
+    <div className='big_container'>
+      <h1>Landing</h1>
+    </div>
+  )
+}
+export default Landing;
